36ed152134411f79eeee93d0a0dd6cefa6b67a90
Nicolas TERRONE <nicolasterrone@gmail.com>
2019-01-24 13:20:47 +0100

    Made User Interface, added tableView and custom cell

---
+++ coder_swag/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  coder_swag
+//
+//  Created by Nicolas Terrone on 24/01/2019.
+//  Copyright © 2019 Nicolas Terrone. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var imageCell: UIImageView!
+    @IBOutlet weak var lblCell: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
