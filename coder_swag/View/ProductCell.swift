59c42f81eaefdd1f8235817f09a04868dfbc6214
Nicolas TERRONE <nicolasterrone@gmail.com>
2019-01-25 12:33:25 +0100

    Added new ProductViewController, collectionView and prepared ProductCollectionCell

---
+++ coder_swag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coder_swag
+//
+//  Created by Nicolas Terrone on 25/01/2019.
+//  Copyright © 2019 Nicolas Terrone. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
